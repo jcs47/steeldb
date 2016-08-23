6df0e0cbd1e579e580fcd370170e64a12a11eb49
snakejerusalem <jcs@lasige.di.fc.ul.pt>
2016-08-23 22:18:47 +0100

    Updated script and library.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-cp -v ../../MerkleTree/dist/MerkleTree.jar lib/
+rm config/currentView
+cp -v ~/NetBeansProjects/MerkleTree/dist/MerkleTree.jar lib/
 rm -R bin/*
 ant
 cp -v bin/SteelDB.jar ../RUBiS/Servlet_HTML/WEB-INF/lib/
-cp -v ../../MerkleTree/dist/MerkleTree.jar ../RUBiS/Servlet_HTML/WEB-INF/lib/
+cp -v ~/NetBeansProjects/MerkleTree/dist/MerkleTree.jar ../RUBiS/Servlet_HTML/WEB-INF/lib/
